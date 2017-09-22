7bc8a81e1faa952b699a66e16e4fba55437ee2d3
ninja_koder <grinders@tlen.pl>
2017-09-23 00:58:35 +0200

    Zakomentowany scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Podaj swoje imię');
+// alert('Witaj, ' + name);
+
+/*
+        Teraz za wiele to
+        tutaj się nie dzieje
+*/
+//      No i kolejny komentarz singiel
+
+console.log('Witaj, ' +name);
